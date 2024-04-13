3106d465acde18bceec0831b05ad37d36c63ed5b
Krunal022 <krunalvaghela723@gmail.com>
2024-04-13 19:29:46 +0530

    add smooth scrolling

---
+++ script.js
@@ -0,0 +1,9 @@
+//locomotive js- smooth scrolling
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+//gsap to animate
+//shery js to animate images according
+
