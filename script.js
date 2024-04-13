c953885e1f28f41c28436f9473c609c32af56756
Krunal022 <krunalvaghela723@gmail.com>
2024-04-13 20:02:42 +0530

    Adding SheryJs & animated heading

--- script.js
+++ script.js
@@ -11,7 +11,15 @@ gsap.from(".nlink",{
     duration:1,
     ease:Power2,
     opacity:0
-
 })
+
+Shery.textAnimate("#heading h1" , {
+    style: 1,
+    y: 10,
+    delay: 0.1,
+    duration: 2,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
+  });
 //shery js to animate images according
 
