145d721855273e17357d86bc7b531f88538af193
Krunal022 <krunalvaghela723@gmail.com>
2024-04-17 10:34:08 +0530

    effects & mousefollower

--- script.js
+++ script.js
@@ -11,15 +11,34 @@ gsap.from(".nlink",{
     duration:1,
     ease:Power2,
     opacity:0
-})
+});
 
 Shery.textAnimate("#heading h1" , {
     style: 1,
     y: 10,
     delay: 0.1,
-    duration: 2,
+    duration: 1,
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     multiplier: 0.1,
   });
+
+gsap.from(".anim2", {
+    stagger:.3,
+    y:50,
+    duration:1,
+    ease:Expo,
+    opacity:0
+});
+
+Shery.mouseFollower({
+  skew: true,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: .5,
+});
+
+Shery.imageEffect("#imgntext img",{
+    style: 4,
+    debug: true
+});
 //shery js to animate images according
 
